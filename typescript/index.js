d2b40f94ab1364e2dabad8be3a2899f4194c23d5
AvusalaChetan <avusalachetan@gmail.com>
2026-08-17 20:57:24 +0530

    ts

---
+++ typescript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello, TypeScript!");
+const a = "4999";
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ typescript/index.ts
@@ -0,0 +1,4 @@
+console.log("Hello, TypeScript!");
+const a: string  = "4999";
+
+console.log(a);
